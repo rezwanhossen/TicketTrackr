288915e63bf2ed7619864e75c11d6754bdc246d9
rezwanhossen <rezwanhossen625@gmail.com>
2026-03-04 22:14:41 +0600

    banner section add

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,17 +5,41 @@ import vectoe2 from "/Clip path group.png";
 const Banner = () => {
   return (
     <div className=" grid grid-cols-1 md:grid-cols-2 gap-5 my-5">
-      <div className=" hero min-h-75 rounded-xl relative bg-linear-to-r from-[#632EE3] to-[#9F62F2] overflow-hidden">
+      <div className=" hero min-h-60 rounded-xl relative bg-linear-to-r from-[#632EE3] to-[#9F62F2] overflow-hidden">
         <img
           src={vectoe1}
-          className=" absolute left-0 top-0 w-full h-full "
+          className=" absolute left-0 top-0 w-[40%] h-full object-cover "
           alt="shap"
         />
         <img
           src={vectoe2}
-          className=" absolute right-0 top-0 w-full h-full"
+          className=" absolute right-0 top-0 w-[40%] h-full object-cover"
           alt="shap"
         />
+        <div className=" hero-content text-center relative z-10">
+          <div>
+            <h1 className=" text-2xl font-bold">In-Progress</h1>
+            <h1 className=" text-5xl font-bold">0</h1>
+          </div>
+        </div>
+      </div>
+      <div className=" hero min-h-60 rounded-xl relative bg-linear-to-r from-[#54CF68] to-[#00827A] overflow-hidden">
+        <img
+          src={vectoe1}
+          className=" absolute left-0 top-0 w-[40%] h-full object-cover  pointer-events-none "
+          alt="shap"
+        />
+        <img
+          src={vectoe2}
+          className=" absolute right-0 top-0 w-[40%] h-full object-cover pointer-events-none"
+          alt="shap"
+        />
+        <div className=" hero-content text-center relative z-10">
+          <div>
+            <h1 className=" text-2xl font-bold">Resolved</h1>
+            <h1 className=" text-5xl font-bold">0</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
